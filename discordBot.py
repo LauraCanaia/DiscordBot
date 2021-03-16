572e0241608b0800a428f1b740a0de84f21024aa
Laura Canaia <lauracanaia99@gmail.com>
2021-03-16 16:43:02 +0100

    Wort It

--- discordBot.py
+++ discordBot.py
@@ -7,21 +7,31 @@ from discord.utils import get
 load_dotenv()
 
 class MyClient(discord.Client):
-	async def on_ready(self):
-		print('Logged on as {0}!'.format(self.user))
-		await self.get_channel(821334437324914690).send('Welcome on CoinFlip Bot OwO\nIf you wanna know all the commands of this bot digit $help')
 	async def on_message(self, message):
 		if message.content.startswith('$help'):
 			await message.channel.send('\nHELP:\n$yo = sends you a yo back,\n$flip = flips a coin\n')
-		if message.content.startswith('$yo'):
-			#await message.channel.send(self.get_user('163718663491420160').name)
-			await message.channel.send('\nHELP:\n$yo = sends you a yo back,\n$flip = flips a coin\n')
-		if message.content.startswith('$flip'):
-			number = random.randint(0,1)
-			if(number == 0):
-				await message.channel.send("Se sei ronco hai perso")
+			
+		if message.content.startswith('$flip h'):
+			ronco = await client.fetch_user("238685089255391233")
+			if message.author == ronco:
+				await message.channel.send("Tails! YOU LOST!")
 			else:
-				await message.channel.send("Head")
-
+				number = random.randint(0,1)
+				if(number == 0):
+					await message.channel.send("Tails! YOU LOST!")
+				else:
+					await message.channel.send("Head! YOU WON!!")
+			
+		if message.content.startswith('$flip t'):
+			ronco = await client.fetch_user("238685089255391233")
+			if message.author == ronco:
+				await message.channel.send("Head! YOU LOST!!")
+			else:
+				number = random.randint(0,1)
+				if(number == 0):
+					await message.channel.send("Tails! YOU WON!!")
+				else:
+					await message.channel.send("Head! YOU LOST!")
+				
 client = MyClient()
 client.run(os.environ.get("KEY"))
